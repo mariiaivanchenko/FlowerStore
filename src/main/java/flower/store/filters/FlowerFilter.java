d1b643798fcda0d8283734f69410c4b1935eca49
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2023-10-12 17:42:20 +0300

    Update FlowerFilter.java

--- src/main/java/flower/store/filters/FlowerFilter.java
+++ src/main/java/flower/store/filters/FlowerFilter.java
@@ -1,10 +1,31 @@
 package flower.store.filters;
 
+import flower.store.item.Flower;
+import flower.store.item.FlowerColor;
+import flower.store.item.FlowerType;
 import flower.store.item.Item;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class FlowerFilter implements SearchFilter {
+    private double sepalLength;
+    private double price;
+    private FlowerColor color;
+    private FlowerType flowerType;
+
     @Override
     public boolean match(Item item) {
-        return true;
+        if (item instanceof Flower) {
+            Flower flower = (Flower) item;
+            if (this.sepalLength == flower.getSepalLength() && this.price == flower.getPrice() && this.color == flower.getColor() && this.flowerType == flower.getFlowerType()) {
+                return true;
+            } else {
+                return false;}
+        }
+        else {
+            return false;}
+        
     }
 }
